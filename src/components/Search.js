9a1864732515ee14b5d0149799714d8e26b535a9
Neha Joshi <nehajoshigkp@gmail.com>
2022-04-10 15:45:30 +0530

    search comp css

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,28 @@
-import {View, TextInput} from 'react-native'
+import {View, TextInput, StyleSheet} from 'react-native';
+import {FontAwesome} from "@expo/vector-icons";
 
 export default function Search() {
     return (
-        <View>
+        <View style={styles.container}>
+            <FontAwesome name="search" size={25}/>
             <TextInput
             placeholder="Restaurants, food"
             />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        marginTop: 5,
+        marginHorizontal: 25,
+        backgroundColor: "white",
+        shadowColor: "black",
+        shadowOffset: { width: 5, height:5 },
+        elevation: 3,
+        shadowOpacity: 0.1,
+        padding: 15,
+        borderRadius: 40,
+    },
+})
